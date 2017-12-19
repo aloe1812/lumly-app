71a029ba5fee19db7cd50adba09b366d535833c0
Eugene Krakos <krakosevich93@gmail.com>
2017-12-19 16:16:34 +0300

    text fixes

--- src/app/header/header/header.component.ts
+++ src/app/header/header/header.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ElectronService } from '../../core/electron.service';
 import { StoreService } from '../../core/store.service';
 import { ProjectService } from '../../core/project.service';
 
@@ -10,12 +9,10 @@ import { ProjectService } from '../../core/project.service';
 })
 export class HeaderComponent implements OnInit {
 
-  project;
   isProjectHasChanges = false;
-  openProjectTitle = 'Open Project';
+  activeProjectTitle = '';
 
   constructor(
-    private electronService: ElectronService,
     private store: StoreService,
     private projectService: ProjectService
   ) { }
@@ -25,12 +22,8 @@ export class HeaderComponent implements OnInit {
     this.subscribeToActiveProject();
   }
 
-  openProject() {
-    if (this.project) {
-      this.store.event('Projects:List').emit();
-    } else {
-      this.projectService.openProject();
-    }
+  openProjects() {
+    this.store.event('Projects:List').emit();
   }
 
   saveProject() {
@@ -46,8 +39,7 @@ export class HeaderComponent implements OnInit {
   private subscribeToActiveProject() {
     this.store.data('Project:Active').get().subscribe(
       (project) => {
-        this.project = project;
-        this.openProjectTitle = project ? project.project.title : 'Open Project';
+        this.activeProjectTitle = project ? project.project.title : '';
       }
     )
   }
