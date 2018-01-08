03404ab06bb7925c0ef8a0ef201298747af0af8c
Eugene Krakos <krakosevich93@gmail.com>
2018-01-08 08:23:34 +0300

    editor: header

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SvgIconModule } from 'app/svg-icon/svg-icon.module';
 import { HeaderComponent } from './header/header.component';
-import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 import { ProjectStatusComponent } from './project-status/project-status.component';
 import { LayoutComponent } from './layout/layout.component';
 
@@ -11,7 +10,7 @@ import { LayoutComponent } from './layout/layout.component';
     CommonModule,
     SvgIconModule
   ],
-  declarations: [HeaderComponent, BreadcrumbsComponent, ProjectStatusComponent, LayoutComponent],
+  declarations: [HeaderComponent, ProjectStatusComponent, LayoutComponent],
   exports: [
     HeaderComponent
   ]
